4b736e60fd805c2b1c2d9807d5a8ee0c26bcb684
joysonjson <joysonpathra@gmail.com>
2022-06-07 23:17:05 +0530

    intial commit

---
+++ PhotoEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoEditor
+//
+//  Created by Joyson P S on 07/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PhotoEditor' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PhotoEditor
+  pod 'QCropper' 
+end
